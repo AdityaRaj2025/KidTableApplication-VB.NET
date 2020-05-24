6abcf395cfec800e8f6fd128506f3669890138f4
ProgrammerAdityaRaj <ar19971@outlook.com>
2020-05-24 16:45:18 +0530

    first commit

---
+++ KidTableApplication/KidTableApplication.Designer.vb
@@ -0,0 +1,391 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class KidTableApplication
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(KidTableApplication))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btn20 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btn15 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btn10 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.txtNumberDisplay = New System.Windows.Forms.TextBox()
+        Me.btn17 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn18 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn19 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn16 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn12 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn13 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn14 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn11 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoSize = True
+        Me.Panel1.BackColor = System.Drawing.Color.Red
+        Me.Panel1.Controls.Add(Me.btn20)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.btn15)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.btn10)
+        Me.Panel1.Controls.Add(Me.btn5)
+        Me.Panel1.Controls.Add(Me.txtNumberDisplay)
+        Me.Panel1.Controls.Add(Me.btn17)
+        Me.Panel1.Controls.Add(Me.btn1)
+        Me.Panel1.Controls.Add(Me.btn18)
+        Me.Panel1.Controls.Add(Me.btn4)
+        Me.Panel1.Controls.Add(Me.btn19)
+        Me.Panel1.Controls.Add(Me.btn3)
+        Me.Panel1.Controls.Add(Me.btn16)
+        Me.Panel1.Controls.Add(Me.btn2)
+        Me.Panel1.Controls.Add(Me.btn12)
+        Me.Panel1.Controls.Add(Me.btn6)
+        Me.Panel1.Controls.Add(Me.btn13)
+        Me.Panel1.Controls.Add(Me.btn9)
+        Me.Panel1.Controls.Add(Me.btn14)
+        Me.Panel1.Controls.Add(Me.btn8)
+        Me.Panel1.Controls.Add(Me.btn11)
+        Me.Panel1.Controls.Add(Me.btn7)
+        Me.Panel1.Location = New System.Drawing.Point(12, 21)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(488, 480)
+        Me.Panel1.TabIndex = 20
+        '
+        'btn20
+        '
+        Me.btn20.BackColor = System.Drawing.Color.Blue
+        Me.btn20.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn20.Location = New System.Drawing.Point(366, 428)
+        Me.btn20.Name = "btn20"
+        Me.btn20.Size = New System.Drawing.Size(78, 34)
+        Me.btn20.TabIndex = 39
+        Me.btn20.Text = "20"
+        Me.btn20.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(152, 294)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(164, 16)
+        Me.Label3.TabIndex = 21
+        Me.Label3.Text = "Please Prees  Number"
+        '
+        'btn15
+        '
+        Me.btn15.BackColor = System.Drawing.Color.Blue
+        Me.btn15.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn15.Location = New System.Drawing.Point(366, 392)
+        Me.btn15.Name = "btn15"
+        Me.btn15.Size = New System.Drawing.Size(78, 30)
+        Me.btn15.TabIndex = 38
+        Me.btn15.Text = "15"
+        Me.btn15.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Harlow Solid Italic", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.GreenYellow
+        Me.Label2.Location = New System.Drawing.Point(151, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(159, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Multiplication Table"
+        '
+        'btn10
+        '
+        Me.btn10.BackColor = System.Drawing.Color.Blue
+        Me.btn10.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn10.Location = New System.Drawing.Point(366, 351)
+        Me.btn10.Name = "btn10"
+        Me.btn10.Size = New System.Drawing.Size(78, 34)
+        Me.btn10.TabIndex = 37
+        Me.btn10.Text = "10"
+        Me.btn10.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Blue
+        Me.btn5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(366, 312)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(78, 33)
+        Me.btn5.TabIndex = 36
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'txtNumberDisplay
+        '
+        Me.txtNumberDisplay.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtNumberDisplay.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtNumberDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumberDisplay.ForeColor = System.Drawing.Color.White
+        Me.txtNumberDisplay.Location = New System.Drawing.Point(20, 23)
+        Me.txtNumberDisplay.Multiline = True
+        Me.txtNumberDisplay.Name = "txtNumberDisplay"
+        Me.txtNumberDisplay.Size = New System.Drawing.Size(450, 257)
+        Me.txtNumberDisplay.TabIndex = 5
+        '
+        'btn17
+        '
+        Me.btn17.BackColor = System.Drawing.Color.Blue
+        Me.btn17.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn17.Location = New System.Drawing.Point(119, 428)
+        Me.btn17.Name = "btn17"
+        Me.btn17.Size = New System.Drawing.Size(78, 34)
+        Me.btn17.TabIndex = 35
+        Me.btn17.Text = "17"
+        Me.btn17.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Blue
+        Me.btn1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(37, 312)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(76, 33)
+        Me.btn1.TabIndex = 20
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn18
+        '
+        Me.btn18.BackColor = System.Drawing.Color.Blue
+        Me.btn18.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn18.Location = New System.Drawing.Point(203, 428)
+        Me.btn18.Name = "btn18"
+        Me.btn18.Size = New System.Drawing.Size(75, 34)
+        Me.btn18.TabIndex = 34
+        Me.btn18.Text = "18"
+        Me.btn18.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Blue
+        Me.btn4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(284, 312)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(76, 33)
+        Me.btn4.TabIndex = 21
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn19
+        '
+        Me.btn19.BackColor = System.Drawing.Color.Blue
+        Me.btn19.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn19.Location = New System.Drawing.Point(284, 428)
+        Me.btn19.Name = "btn19"
+        Me.btn19.Size = New System.Drawing.Size(76, 34)
+        Me.btn19.TabIndex = 33
+        Me.btn19.Text = "19"
+        Me.btn19.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Blue
+        Me.btn3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(203, 312)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(75, 33)
+        Me.btn3.TabIndex = 22
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn16
+        '
+        Me.btn16.BackColor = System.Drawing.Color.Blue
+        Me.btn16.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn16.Location = New System.Drawing.Point(37, 428)
+        Me.btn16.Name = "btn16"
+        Me.btn16.Size = New System.Drawing.Size(76, 34)
+        Me.btn16.TabIndex = 32
+        Me.btn16.Text = "16"
+        Me.btn16.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Blue
+        Me.btn2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(119, 312)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(78, 33)
+        Me.btn2.TabIndex = 23
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn12
+        '
+        Me.btn12.BackColor = System.Drawing.Color.Blue
+        Me.btn12.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn12.Location = New System.Drawing.Point(119, 392)
+        Me.btn12.Name = "btn12"
+        Me.btn12.Size = New System.Drawing.Size(78, 30)
+        Me.btn12.TabIndex = 31
+        Me.btn12.Text = "12"
+        Me.btn12.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Blue
+        Me.btn6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(37, 351)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(76, 34)
+        Me.btn6.TabIndex = 24
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn13
+        '
+        Me.btn13.BackColor = System.Drawing.Color.Blue
+        Me.btn13.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn13.Location = New System.Drawing.Point(203, 392)
+        Me.btn13.Name = "btn13"
+        Me.btn13.Size = New System.Drawing.Size(75, 30)
+        Me.btn13.TabIndex = 30
+        Me.btn13.Text = "13"
+        Me.btn13.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Blue
+        Me.btn9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(284, 351)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(76, 34)
+        Me.btn9.TabIndex = 25
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn14
+        '
+        Me.btn14.BackColor = System.Drawing.Color.Blue
+        Me.btn14.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn14.Location = New System.Drawing.Point(284, 392)
+        Me.btn14.Name = "btn14"
+        Me.btn14.Size = New System.Drawing.Size(76, 30)
+        Me.btn14.TabIndex = 29
+        Me.btn14.Text = "14"
+        Me.btn14.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Blue
+        Me.btn8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(203, 351)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(75, 34)
+        Me.btn8.TabIndex = 26
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn11
+        '
+        Me.btn11.BackColor = System.Drawing.Color.Blue
+        Me.btn11.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn11.Location = New System.Drawing.Point(37, 392)
+        Me.btn11.Name = "btn11"
+        Me.btn11.Size = New System.Drawing.Size(76, 30)
+        Me.btn11.TabIndex = 28
+        Me.btn11.Text = "11"
+        Me.btn11.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Blue
+        Me.btn7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(119, 351)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(78, 34)
+        Me.btn7.TabIndex = 27
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btnClose
+        '
+        Me.btnClose.BackColor = System.Drawing.Color.Yellow
+        Me.btnClose.Location = New System.Drawing.Point(207, 507)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 21
+        Me.btnClose.Text = "Close App"
+        Me.btnClose.UseVisualStyleBackColor = False
+        '
+        'KidTableApplication
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(518, 533)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "KidTableApplication"
+        Me.Text = "Kid Table Application"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtNumberDisplay As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btn20 As System.Windows.Forms.Button
+    Friend WithEvents btn15 As System.Windows.Forms.Button
+    Friend WithEvents btn10 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn17 As System.Windows.Forms.Button
+    Friend WithEvents btn18 As System.Windows.Forms.Button
+    Friend WithEvents btn19 As System.Windows.Forms.Button
+    Friend WithEvents btn16 As System.Windows.Forms.Button
+    Friend WithEvents btn12 As System.Windows.Forms.Button
+    Friend WithEvents btn13 As System.Windows.Forms.Button
+    Friend WithEvents btn14 As System.Windows.Forms.Button
+    Friend WithEvents btn11 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+
+End Class
---
+++ KidTableApplication/KidTableApplication.vb
@@ -0,0 +1,23 @@
+﻿Public Class KidTableApplication
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub GenerateTable(sender As Object, e As EventArgs) Handles btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click, btn10.Click, btn11.Click, btn12.Click, btn13.Click, btn14.Click, btn15.Click, btn16.Click, btn17.Click, btn18.Click, btn19.Click, btn20.Click
+        Dim b As Button = sender
+        Dim prod As Integer
+        txtNumberDisplay.Clear()
+
+        For i As Integer = 1 To 10
+            prod = b.Text * i
+            txtNumberDisplay.Text = txtNumberDisplay.Text & b.Text & " × " & i & " = " & prod & vbCrLf
+        Next
+    End Sub
+
+
+    Private Sub KidTableApplication_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        Panel1.Width = Me.Width - 15
+        Panel1.Height = Me.Height - 15
+        txtNumberDisplay.Width = Me.Width - 70
+    End Sub
+End Class
